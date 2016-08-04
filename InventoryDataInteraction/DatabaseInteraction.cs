6424cb8714c88c3a55ea07e38b6756e22217bdc1
McSketchy <gqweasle@gmail.com>
2016-08-03 19:27:48 -0600

    Added DataMapping Project

--- InventoryDataInteraction/DatabaseInteraction.cs
+++ InventoryDataInteraction/DatabaseInteraction.cs
@@ -1,225 +1,225 @@
 ﻿using InventoryData;
 using InventoryDataInteraction;
-//using InventoryDataMapping.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using InventoryDataMapping.Models;
 
 namespace InventoryDataInteraction
 {
 
 	public class DatabaseInteraction //: IInventoryData, IUserRepository
 	{
-		
-
-		//public IEnumerable<Order> GetOrders()
-		//{
-		//	using(var db = new InventoryContext())
-		//	{
-		//		return (from order in db.Orders.Include("OrderItems")
-		//									   .Include("OrderItems.Item")
-		//									   .Include("Purchaser")
-		//				select order).ToList();
-		//	}
-		//}
-
-		//public IEnumerable<Item> GetItems()
-		//{
-		//	using (var db = new InventoryContext())
-		//	{
-		//		return (from item in db.Items
-		//				select item).ToList();
-		//	}
-		//}
-
-		//public IEnumerable<OrderItem> GetOrderItems(int orderNumber)
-		//{
-		//	using (var db = new InventoryContext())
-		//	{
-		//		return (from orderItem in db.OrderItems
-		//				where orderItem.OrderNumber == orderNumber
-		//				select orderItem).ToList();
-		//	}
-		//}
-
-		//public bool SaveOrder(Order order)
-		//{
-		//	if (order == null)
-		//		return false;
-
-		//	try
-		//	{
-		//		using (var db = new InventoryContext())
-		//		{
-		//			db.Orders.Add(order);
-		//			db.SaveChanges();
-		//		}
-		//		return true;
-		//	}
-		//	catch (Exception e)
-		//	{
-		//		return false;
-		//	}
-		//}
-
-		//public bool SaveItem(Item item)
-		//{
-		//	if (item == null)
-		//		return false;
-
-		//	try
-		//	{
-		//		using (var db = new InventoryContext())
-		//		{
-		//			db.Items.Add(item);
-		//			db.SaveChanges();
-		//		}
-		//		return true;
-		//	}
-		//	catch(Exception e)
-		//	{
-		//		return false;
-		//	}
-		//}
-
-		//public bool SaveOrderItem(int orderNumber, OrderItem orderItem)
-		//{
-		//	if (orderItem == null)
-		//		return false;
-
-		//	try
-		//	{
-		//		using (var db = new InventoryContext())
-		//		{
-		//			var order = (from o in db.Orders
-		//						 where o.OrderNumber == orderNumber
-		//						 select o).SingleOrDefault();
-
-		//			if (order == null)
-		//				return false;
-
-		//			order.OrderItems.Add(orderItem);
-		//			db.OrderItems.Add(orderItem);
-		//			db.SaveChanges();
-		//		}
-
-		//		return true;
-		//	}
-		//	catch(Exception e)
-		//	{
-		//		return false;
-		//	}
-		//}
-
-		//public IEnumerable<User> GetUsers()
-		//{
-		//	using (var db = new InventoryContext())
-		//	{
-		//		return (from user in db.Users
-		//				select user).ToList();
-		//	}
-		//}
-
-		//public bool SaveUser(User user)
-		//{
-		//	if (user == null)
-		//		return false;
-
-		//	try
-		//	{
-		//		using (var db = new InventoryContext())
-		//		{
-		//			db.Users.Add(user);
-		//			db.SaveChanges();
-		//		}
-		//		return true;
-		//	}
-		//	catch (Exception e)
-		//	{
-		//		return false;
-		//	}
-		//}
-
-		//public IEnumerable<Order> GetUserOrders(int userId)
-		//{
-		//	using (var db = new InventoryContext())
-		//	{
-		//		return (from u in db.UserOrders
-		//				where u.UserId == userId
-		//				select u.Order).ToList();
-		//	}
-		//}
 
 
-	//	public Task<IEnumerable<Order>> GetOrdersAsync()
-	//	{
-	//		var factory = new TaskFactory<IEnumerable<Order>>();
-	//		return factory.StartNew(() =>
-	//		{
-	//			return this.GetOrders();
-	//		});
-	//	}
+    //    public ienumerable<order> getorders()
+    //    {
+    //        using (var db = new inventorycontext())
+    //        {
+    //            return (from order in db.orders.include("orderitems")
+    //                                           .include("orderitems.item")
+    //                                           .include("purchaser")
+    //                    select order).tolist();
+    //        }
+    //    }
 
-	//	public Task<IEnumerable<Item>> GetItemsAsync()
-	//	{
-	//		var factory = new TaskFactory<IEnumerable<Item>>();
-	//		return factory.StartNew(() =>
-	//		{
-	//			return GetItems();
-	//		});
-	//	}
+    //    public ienumerable<item> getitems()
+    //    {
+    //        using (var db = new inventorycontext())
+    //        {
+    //            return (from item in db.items
+    //                    select item).tolist();
+    //        }
+    //    }
 
-	//	public Task<IEnumerable<OrderItem>> GetOrderItemsAsync(int orderNumber)
-	//	{
-	//		var factory = new TaskFactory<IEnumerable<OrderItem>>();
-	//		return factory.StartNew(() =>
-	//		{
-	//			return GetOrderItems(orderNumber);
-	//		});
-	//	}
+    //    public ienumerable<orderitem> getorderitems(int ordernumber)
+    //    {
+    //        using (var db = new inventorycontext())
+    //        {
+    //            return (from orderitem in db.orderitems
+    //                    where orderitem.ordernumber == ordernumber
+    //                    select orderitem).tolist();
+    //        }
+    //    }
 
-	//	public Task<bool> SaveOrderAsync(Order order)
-	//	{
-	//		var factory = new TaskFactory<bool>();
-	//		return factory.StartNew(() =>
-	//		{
-	//			return SaveOrder(order);
-	//		});
-	//	}
+    //    public bool saveorder(order order)
+    //    {
+    //        if (order == null)
+    //            return false;
 
-	//	public Task<bool> SaveItemAsync(Item item)
-	//	{
-	//		var factory = new TaskFactory<bool>();
-	//		return factory.StartNew(() =>
-	//		{
-	//			return SaveItem(item);
-	//		});
-	//	}
+    //        try
+    //        {
+    //            using (var db = new inventorycontext())
+    //            {
+    //                db.orders.add(order);
+    //                db.savechanges();
+    //            }
+    //            return true;
+    //        }
+    //        catch (exception e)
+    //        {
+    //            return false;
+    //        }
+    //    }
 
-	//	public Task<bool> SaveOrderItemAsync(int orderNumber, OrderItem orderItem)
-	//	{
-	//		var factory = new TaskFactory<bool>();
-	//		return factory.StartNew(() =>
-	//		{
-	//			return SaveOrderItem(orderNumber, orderItem);
-	//		});
-	//	}
+    //    public bool saveitem(item item)
+    //    {
+    //        if (item == null)
+    //            return false;
 
-	//	public Task<IEnumerable<User>> GetUsersAsync()
-	//	{
-	//		return new TaskFactory<IEnumerable<User>>().StartNew(this.GetUsers);
-	//	}
+    //        try
+    //        {
+    //            using (var db = new inventorycontext())
+    //            {
+    //                db.items.add(item);
+    //                db.savechanges();
+    //            }
+    //            return true;
+    //        }
+    //        catch (exception e)
+    //        {
+    //            return false;
+    //        }
+    //    }
 
-	//	public Task<bool> SaveUserAsync(User user)
-	//	{
-	//		return new TaskFactory<bool>().StartNew(() => this.SaveUser(user));
-	//	}
+    //    public bool saveorderitem(int ordernumber, orderitem orderitem)
+    //    {
+    //        if (orderitem == null)
+    //            return false;
 
-	//	public Task<IEnumerable<Order>> GetUserOrdersAsync(int userId)
-	//	{
-	//		return new TaskFactory<IEnumerable<Order>>().StartNew(() => this.GetUserOrders(userId));
-	//	}
-	}
+    //        try
+    //        {
+    //            using (var db = new inventorycontext())
+    //            {
+    //                var order = (from o in db.orders
+    //                             where o.ordernumber == ordernumber
+    //                             select o).singleordefault();
+
+    //                if (order == null)
+    //                    return false;
+
+    //                order.orderitems.add(orderitem);
+    //                db.orderitems.add(orderitem);
+    //                db.savechanges();
+    //            }
+
+    //            return true;
+    //        }
+    //        catch (exception e)
+    //        {
+    //            return false;
+    //        }
+    //    }
+
+    //    public ienumerable<user> getusers()
+    //    {
+    //        using (var db = new inventorycontext())
+    //        {
+    //            return (from user in db.users
+    //                    select user).tolist();
+    //        }
+    //    }
+
+    //    public bool saveuser(user user)
+    //    {
+    //        if (user == null)
+    //            return false;
+
+    //        try
+    //        {
+    //            using (var db = new inventorycontext())
+    //            {
+    //                db.users.add(user);
+    //                db.savechanges();
+    //            }
+    //            return true;
+    //        }
+    //        catch (exception e)
+    //        {
+    //            return false;
+    //        }
+    //    }
+
+    //    public ienumerable<order> getuserorders(int userid)
+    //    {
+    //        using (var db = new inventorycontext())
+    //        {
+    //            return (from u in db.userorders
+    //                    where u.userid == userid
+    //                    select u.order).tolist();
+    //        }
+    //    }
+
+
+    //    public task<ienumerable<order>> getordersasync()
+    //    {
+    //        var factory = new taskfactory<ienumerable<order>>();
+    //        return factory.startnew(() =>
+    //        {
+    //            return this.getorders();
+    //        });
+    //    }
+
+    //    public task<ienumerable<item>> getitemsasync()
+    //    {
+    //        var factory = new taskfactory<ienumerable<item>>();
+    //        return factory.startnew(() =>
+    //        {
+    //            return getitems();
+    //        });
+    //    }
+
+    //    public task<ienumerable<orderitem>> getorderitemsasync(int ordernumber)
+    //    {
+    //        var factory = new taskfactory<ienumerable<orderitem>>();
+    //        return factory.startnew(() =>
+    //        {
+    //            return getorderitems(ordernumber);
+    //        });
+    //    }
+
+    //    public task<bool> saveorderasync(order order)
+    //    {
+    //        var factory = new taskfactory<bool>();
+    //        return factory.startnew(() =>
+    //        {
+    //            return saveorder(order);
+    //        });
+    //    }
+
+    //    public task<bool> saveitemasync(item item)
+    //    {
+    //        var factory = new taskfactory<bool>();
+    //        return factory.startnew(() =>
+    //        {
+    //            return saveitem(item);
+    //        });
+    //    }
+
+    //    public task<bool> saveorderitemasync(int ordernumber, orderitem orderitem)
+    //    {
+    //        var factory = new taskfactory<bool>();
+    //        return factory.startnew(() =>
+    //        {
+    //            return saveorderitem(ordernumber, orderitem);
+    //        });
+    //    }
+
+    //    public task<ienumerable<user>> getusersasync()
+    //    {
+    //        return new taskfactory<ienumerable<user>>().startnew(this.getusers);
+    //    }
+
+    //    public task<bool> saveuserasync(user user)
+    //    {
+    //        return new taskfactory<bool>().startnew(() => this.saveuser(user));
+    //    }
+
+    //    public task<ienumerable<order>> getuserordersasync(int userid)
+    //    {
+    //        return new taskfactory<ienumerable<order>>().startnew(() => this.getuserorders(userid));
+    //    }
+    }
 }
