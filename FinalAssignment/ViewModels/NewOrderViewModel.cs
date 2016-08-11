9ac0629ca26b98c5b3ed01b1cae70d4ce6cf5589
Sean O'Neil <Sean O'Neil@DESKTOP-V4GB1PC>
2016-08-11 08:57:26 -0600

    Here is just a basic template of the NewOrder props awaiting a DB interaction method to adjust accordingly

--- FinalAssignment/ViewModels/NewOrderViewModel.cs
+++ FinalAssignment/ViewModels/NewOrderViewModel.cs
@@ -1,26 +1,9 @@
 ﻿
 
-
-
-
 //I added DisplayMemberPath variable to the Purchaser ComboBox so it would show the Name property
 //of the User Objects it was listing. I also did the database hookup for it so you could get an idea
 //of how to do it for the other things. Also including a few notes further down to help guide you a bit
 //It's all guess work on my end so good luck :P
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 using Caliburn.Micro;
@@ -35,7 +18,7 @@ using System.Collections.ObjectModel;
 
 namespace FinalAssignment.ViewModels
 {
-    class NewOrderViewModel : Screen
+    class NewOrderViewModel : Screen, INotifyPropertyChangedEx
     {
         // Private variable with corresopnding public property
         private ObservableCollection<User> _Purchaser;
@@ -54,10 +37,93 @@ namespace FinalAssignment.ViewModels
         /*TODO: Add variables/properties for OrderNumber, PurchaseDate, OrderTotal, ItemComboBox
          SelectedPurchaser, SelectedItem, and SelectedNewOrderItems*/
 
+        /* Unsure if these properties are needed or even if they will function as is
+         *  
+         * private int OrderNumber;
+        public int _OrderNumber
+        {
+            get
+            {
+                return _OrderNumber;
+            }
+            set
+            {
+                _OrderNumber = value;
+                NotifyOfPropertyChange(() => OrderNumber);
+            }
+        }
 
+        private int PurchaseDate;
+        public int _PurchaseDate
+        {
+            get
+            {
+                return _PurchaseDate;
+            }
+            set
+            {
+                _PurchaseDate = value;
+                NotifyOfPropertyChange(() => PurchaseDate);
+            }
+        }
 
+        private int OrderTotal;
+        public int _OrderTotal
+        {
+            get
+            {
+                return _OrderTotal;
+            }
+            set
+            {
+                _OrderTotal = value;
+                NotifyOfPropertyChange(() => OrderTotal);
+            }
+        }
 
-       
+        private String SelectedPurchaser;
+        public String _SelectedPurchaser
+        {
+            get
+            {
+                return _SelectedPurchaser;
+            }
+            set
+            {
+                _SelectedPurchaser = value;
+                NotifyOfPropertyChange(() => Purchaser);
+            }
+        }
+        */
+
+        private ObservableCollection<Item> ItemComboBox;
+        public ObservableCollection<Item> _ItemComboBox
+        {
+            get
+            {
+                return _ItemComboBox;
+            }
+            set
+            {
+                _ItemComboBox = value;
+                NotifyOfPropertyChange(() => ItemComboBox);
+            }
+        }
+
+        private Item SelectedItemComboBox;
+        public ObservableCollection<Item> _SelectedItemComboBox
+        {
+            get
+            {
+                return _SelectedItemComboBox;
+            }
+            set
+            {
+                _SelectedItemComboBox = value;
+                NotifyOfPropertyChange(() => SelectedItemComboBox);
+            }
+        }
+
         public NewOrderViewModel()
         {
             //DatabaseInteraction Object, check InventoryDataInteraction.DatabaseInteraction.cs
@@ -72,6 +138,8 @@ namespace FinalAssignment.ViewModels
         {
             return true; 
         }
+
+
 
         /*TODO: Populate Item ComboBox 
          AddItem onclick method -- Create OrderItem and add to datagrid (OrderNumber from above, quantity 0, cost from item, name from item)
